cfcf193b03e70d4fc47d6d547c60225775e4fb49
miskron al <241111100@student.unu-jogja.ac.id>
2026-07-13 17:12:03 +0700

    Debug Railway DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -91,9 +91,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
+print("DATABASE_URL =", repr(os.getenv("DATABASE_URL")))
+
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL")
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
 
